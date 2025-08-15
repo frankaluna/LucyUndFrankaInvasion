1af503ee66a158acd0a21cb4e152c209ac6d974b
frankaluna <freericks.franka@gmail.com>
2025-08-15 12:03:28 +0200

    Erste Anfänge der PlayerControl Klassen, viel noch zu machen in cpp

---
+++ Abgabe_Vorlage/src/control/PlayerControl/PlayerControl.cpp
@@ -0,0 +1,32 @@
+#include "PlayerControl.hpp"
+
+PlayerControl::PlayerControl(Layer &layer) : layer(layer) {
+
+}
+
+//moves Player to the right
+void PlayerControl::right_button_pressed() {
+    player.move_right();
+} 
+
+//moves Player to the left
+void PlayerControl::left_button_pressed() {
+    player.move_left();
+}
+
+//shoots if spacebar is pressed
+void PlayerControl::spacebar_pressed() {
+    player.shoot();
+}
+
+
+//stops 
+void PlayerControl::direction_button_released(HorizontalDirection direction) {
+    if (direction == player.get_horizontal_movement())
+        player.stop_horizontal_movement();
+}
+
+bool PlayerControl::can_shoot(float elapsed_time) {
+    float elapsed_time
+    return (elapsed_time > 0.5);
+}
