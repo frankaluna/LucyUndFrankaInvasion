285ad579505634cf9f4bb5b9580ff8d2dc2116b1
lucymvrie <lucy.hansche@gmail.com>
2025-08-16 22:07:35 +0200

    shields in main

---
+++ Abgabe_Vorlage/src/model/Shields/shields.hpp
@@ -0,0 +1,35 @@
+#ifndef SHIELDS_HPP
+#define ALIEN_HPP
+
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include "Directions.hpp"
+
+//shield model class
+class Shield{
+    public:
+    //constructor and initialize
+    Shield(int lives = 30);
+
+    //position of shield
+    void set_position(float x, float y);
+
+    //shield's horizontal position
+    float get_position_x() const;
+
+    //shield's vertical position
+    float get_position_y() const;
+
+    //sprite for shield
+    const sf:: Sprite&get_sprite() const;
+
+    private:
+    sf:: Texture texture;
+    sf:: Sprite sprite;
+
+    //sprite position
+    float position_x;
+    float position_y;
+};
+#endif
