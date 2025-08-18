9d5a1bb65611b0a3be29de3cb95ea3fe7812d5fb
frankaluna <freericks.franka@gmail.com>
2025-08-18 13:59:02 +0200

    keine Ahnung, was ich noch da geändert hab

--- Abgabe_Vorlage/src/model/Projectile/Projectile.cpp
+++ Abgabe_Vorlage/src/model/Projectile/Projectile.cpp
@@ -1,4 +1,3 @@
 #include "Projectile.hpp"
-#include
 
 Projectile::Projectile(float startY, float speed, Shooter shooter, float radius) : y_position(startY), shooter(this -> shooter), radius(this -> radius), 
