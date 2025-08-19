b0e743ec0394115f06d13cf20abf177aa2f4dc63
lucymvrie <lucy.hansche@gmail.com>
2025-08-19 12:47:46 +0200

    ein wenig playercontrol ergänzt

--- Abgabe_Vorlage/src/control/PlayerControl.hpp
+++ Abgabe_Vorlage/src/control/PlayerControl.hpp
@@ -1,9 +1,9 @@
-#ifndef PLAYERCONTROL_H
-#define PLAYERCONTROL_H
+#ifndef PLAYERCONTROL_HPP
+#define PLAYERCONTROL_HPP
 
 #include <SFML/Graphics.hpp>
 #include "../model/Player.hpp"
-#include "../model/Directions.hpp"
+#include "./Directions.hpp"
 #include "../view/Layer.hpp"
 
 
@@ -25,17 +25,29 @@ class PlayerControl {
     //stop horizontal movement
     void direction_button_released(HorizontalDirection direction);
 
+    //lets the character start shooting (reset shoot timer)
+    void start_shoot();
+
     //checks if character can shoot again
     bool can_shoot(float elapsed_time);
 
     //updates Players position
-    void update();
+    void update(float elapsed_time);
+
+    //draw character to layer
+    void draw_player();
+
+    //get vertical position of the character
+    float get_player_pos() const;
 
 
 private:
-
+    //player
     Player player;
+
+    //layer where player is drawn
     Layer &layer;
+
 
 
     
