9fb7f4deefe3626ba1e243deea6f0730a33c979b
frankaluna <freericks.franka@gmail.com>
2025-09-03 13:01:42 +0200

    final merge

--- Abgabe_Vorlage/test/GameTest.cpp
+++ Abgabe_Vorlage/test/GameTest.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-// TODO: remove this test and replace with something useful
 TEST(GameTest, example) { 
   // Expect equality.
   EXPECT_EQ(3 * 4, 12);
 }
+
+/*Da die Logik intern in den Control und Model Klassen bereits getestet wird (input, draw & update ) 
+oder es sich um Methoden mit sprites handelt, die wir wie in collisions nicht testen können, ist in dieser Klasse kein Test Bedarf*/
